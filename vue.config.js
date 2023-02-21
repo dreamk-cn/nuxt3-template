2fc8362007be239909091869215a7790256f530e
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-21 21:47:56 +0300

    fix: move output dir to dist for DigitalOcean deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 module.exports = defineConfig({
-  outputDir: path.resolve(__dirname, "docs"),
   transpileDependencies: true,
 });
