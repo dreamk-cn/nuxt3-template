b84b909c3d67c5df732888116f517cb81d313911
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-04-21 20:39:28 +0300

    chore: move index header

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-
+    <div class="hero min-h-screen" style="">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-base-300 dark:text-secondary">
+            <div class="max-w-md">
+                <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                    exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <button class="btn btn-ghost bg-secondary dark:text-primary">Get Started</button>
+            </div>
+        </div>
     </div>
 </template>
 
