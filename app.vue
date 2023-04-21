fc84968271620e2154e7db6089aad1b30b996d48
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-04-21 20:28:54 +0300

    feat: dark mode favicon change

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-
-  </div>
+    <Head>
+        <Link rel="icon" type="image/svg+xml" :href="favicon"></Link>
+    </Head>
+    <Navbar />
+    <div>
+        <NuxtPage />
+    </div>
+    <Footer :is-dark="isDark"/>
 </template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const isDark = computed(() => colorMode.value === 'dark')
+const favicon = computed(() => isDark.value ? '/vmun-white.svg' : '/vmun-black.svg')
+</script>
