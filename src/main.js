f49577d50f059feea295819535e1190b6bcbe12c
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-04-21 17:41:28 +0300

    feat: unhead library

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import { createHead } from '@unhead/vue';
+
 import './style.css'
 import App from './App.vue'
-
 import router from './router/';
 
 const app = createApp(App)
+const head = createHead();
 
 app.use(router)
+app.use(head)
 
 app.mount('#app')
